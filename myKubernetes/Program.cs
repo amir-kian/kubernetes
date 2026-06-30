684636f24c58d6eae84cc61b0e638210954e4d0d
Javaheri, Amirhossein <Javaheri.A@ronixtools.com>
2026-06-30 14:43:36 +0330

    initialise project

---
+++ myKubernetes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace myKubernetes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true) {
+            Console.WriteLine("Current Date and Time: " + DateTime.Now);
+        }
+    }
+
+}
